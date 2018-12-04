0ff9f88419c50fabb725f1981b4dbad888673319
myhrnz1 <myhrnz@outlook.com>
2018-12-04 13:00:44 +0100

    fix bug in checkPassword method

--- MTTK_Login.java
+++ MTTK_Login.java
@@ -18,8 +18,12 @@ public class MTTK_Login {
   }
 
   public boolean checkPassword(String password) {
+    System.out.println(usernameIsValid);
+    System.out.println(tempUsername);
+    System.out.println(password);
     if (usernameIsValid) {
-      return password == passwordOfUser(tempUsername);
+      System.out.println(MTTK_Users.passwordOfUser(tempUsername));
+      return password.equals(MTTK_Users.passwordOfUser(tempUsername));
     }
     return false;
   }

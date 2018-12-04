e96a2d046c0edd84c03a60c5e1e527339d80c0af
myhrnz1 <myhrnz@outlook.com>
2018-12-04 10:30:00 +0100

    implement basic functionality

--- MTTK_Users.java
+++ MTTK_Users.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class MTTK_Users {
+  static ArrayList<MTTK_Users> usersList = new ArrayList<MTTK_Users>();
+
+  String username = "";
+  String password = "";
+
+  private MTTK_Users(String inputUsername, String inputPassword) {
+    username = inputUsername;
+    password = inputPassword;
+  }
+
+  public static void addMTTK_User(String inputUsername, String inputPassword) {
+    MTTK_Users newUser = new MTTK_Users(inputUsername, inputPassword);
+    usersList.add(newUser);
+  }
+
+  public static int indexOfUser(String username) {
+    int index;
+    for (index = 0; index < usersList.size(); index++) {
+      if (usersList.get(index).username.equals(username)) {
+        break;
+      }
+    }
+  return index;
+  }
+
+  public static String passwordOfUser(String username) {
+    int index = indexOfUser(username);
+    String password = "-1";
+    if (index < usersList.size()) {
+      password = usersList.get(index).password;
+    }
+    return password;
+  }
 }
